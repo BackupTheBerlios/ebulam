bd6b8340af753659b1d7d7b4c7564275b13fe277
malube <malube>
2002-05-25 19:28:02 +0000

    Attribute  (InfoMail[], Newsletter[],SemiListe[]) und Get- und Setmethoden geschrieben

--- intarsys/kundenP/KundeBean.java
+++ intarsys/kundenP/KundeBean.java
@@ -1,5 +1,8 @@
-/* $id§ */
+/* $id$ */
 /* $Log: KundeBean.java,v $
+/* Revision 1.3  2002/05/25 19:28:02  malube
+/* Attribute  (InfoMail[], Newsletter[],SemiListe[]) und Get- und Setmethoden geschrieben
+/*
 /* Revision 1.2  2002/05/25 05:07:51  malube
 /* Dateikopf fuer Cvs bei allen java-Files hinzugefuegt
 /* */
@@ -8,6 +11,9 @@ package kundenP;
 
 import java.io.Serializable;
 
+/**
+ * @stereotype entitiy
+ */
 public class KundeBean implements Serializable {
 
 // Attribute
@@ -15,18 +21,31 @@ public class KundeBean implements Serializable {
    private String pass = "";
    private String nachname = "";
    private String vorname = "";
-// Konstruktoren
+   private Adresse anschrift;
+   private String anrede;
+   private String versuch="";
+   private InfMail_Liste[] infomails;
+   private NL_Liste[] newsletters;
+   private Semi_Liste[] seminare;
+   // Konstruktoren
 
    public KundeBean() {
       super();
    }
-   public KundeBean(String userId, String pass) {
+   public KundeBean(String userId, String pass, String vorname, String nachname) {
 
       super();
       this.userId = userId;
       this.pass = pass;
+      this.vorname = vorname;
+      this.nachname = nachname;
    }
-
+   public String getVersuch() {
+      return versuch;
+   }
+   public void setVersuch(String versuch) {
+      this.versuch = versuch;
+   }
    public String getUserId() {
       return userId;
    }
@@ -51,10 +70,50 @@ public class KundeBean implements Serializable {
    public void setVorname(String vorname) {
       this.vorname = vorname;
    }
+   public String getAnrede(){
+            return anrede;
+   }
+
+    public void setAnrede(String anrede){
+            this.anrede = anrede;
+    }
+    public Adresse getAnschrift(){
+            return anschrift;
+    }
+
+    public void setAnschrift(Adresse anschrift){
+            this.anschrift = anschrift;
+    }
+    public InfMail_Liste[] getInfomails(){
+            return infomails;
+    }
+
+    public void setInfomails(InfMail_Liste[] infomails){
+            this.infomails = infomails;
+    }
+
+    public NL_Liste[] getNewsletters(){
+            return newsletters;
+    }
+
+    public void setNewsletters(NL_Liste[] newsletters){
+            this.newsletters = newsletters;
+    }
+
+    public Semi_Liste[] getSeminare(){
+            return seminare;
+    }
+
+    public void setSeminare(Semi_Liste[] seminare){
+            this.seminare = seminare;
+    }
+
 
    public String toString() {
       return "UserId: " + userId + ", Nachname: " + nachname +
-             ", Vorname: " + vorname + ", Passwort: " + pass;
+             ", Vorname: " + vorname + ", Passwort: " + pass +
+             ", Anrede: " + anrede;
+             // Hier fehlen noch die Adresse + die Arrays
    }
    public boolean equals(Object obj) {
       if (obj instanceof KundeBean == false)
@@ -62,5 +121,6 @@ public class KundeBean implements Serializable {
       KundeBean k = (KundeBean) obj;
       return userId == k.userId && nachname.equals(k.nachname) &&
              vorname.equals(k.vorname) && pass.equals(k.pass);
+             // Hier fehlen noch fast alles
    }
 }
